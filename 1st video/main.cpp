4c20c9b96f2c43ce3794dff4f1163a78ff94951c
raosarang20 <79274165+raosarang20@users.noreply.github.com>
2021-05-12 12:45:53 +0530

    Add files via upload

---
+++ 1st video/main.cpp
@@ -0,0 +1,247 @@
+ #include<iostream>
+#include<windows.h>
+
+
+
+
+
+using namespace std;
+
+
+
+void care() {
+
+	cout << " \t\t  Take care guys dont go out.  " << endl;
+
+
+
+}
+
+
+
+void nowear_mask() {
+
+    cout << " \t\t Please wear mask.           " << endl;
+
+cout <<"                                            .--::::-.                                         " << endl;
+cout <<"                                         `. .://://///://`                                    " << endl;
+cout <<"                                  `---. -+-:/://///++/::/-...``                               " << endl;
+cout <<"                                   `:+//////:////o++/::////////:-.`                           " << endl;
+cout <<"                            .---::---++/::///:::///::/++++++++///:/:`                         " << endl;
+cout <<"                          -/://///:://:::/+///:::::::://////////+:://-                        " << endl;
+cout <<"                         ./:/+++//::::::::/+//::::::::::::://++//::::/:`                      " << endl;
+cout <<"                         /+/+o++++/:::::////:////::::::::://////+/::::/:                      " << endl;
+cout <<"                         /+/////o+///::///+/:/++/::/:://///+++++/+:::://                      " << endl;
+cout <<"                      `.://:::::/+//://:../+///++///::/+++////////:::/:`                      " << endl;
+cout <<"                    `://///::::::/::::---../+///++/:::/-/++//:::::/:://`                      " << endl;
+cout <<"                    /+//++/::::://../++++/../++///////o++o+++///////::/:                      " << endl;
+cout <<"                   -++/++++/::/::.``.-----.`.-/+++++//::///++//++++/:::+                      " << endl;
+cout <<"                   .+++/+++//:/.````.-.``.-```..-::-:.``.-.:/+/+/+++///+                      " << endl;
+cout <<"                    -/++///++/:````.:  --.-````````:```  `-`.-::::/+++//                      " << endl;
+cout <<"                     `-/-...:+:.```.- `+o:.`.......:.oo` .-````.--:-://-.                     " << endl;
+cout <<"                      -.`..`..-:-...---::..-..````.-::-.--....-.--.``..`:                     " << endl;
+cout <<"                     `:`.-.````.-```......`          `......`` -.```.:.`:                     " << endl;
+cout <<"                      :.`.-.````:                              :````....-                     " << endl;
+cout <<"                      `.-....```:           wear mask          .-`.-.-..                            " << endl;
+cout <<"                         `..-.`.:                               -.:`                                " << endl;
+cout <<"                            .-.-`           stay safe         `. --                                 " << endl;
+cout <<"                             `:  .-.                         `-``-                                  " << endl;
+cout <<"                              `-``-`-`                       ` `-`                                  " << endl;
+cout <<"                                -. .-..                   `-  .-                                    " << endl;
+cout <<"                                 `-` `..                 `. `-`                                     " << endl;
+cout <<"                                   ..` ``.`              `..`                                       " << endl;
+cout <<"                                     `...` ..`       `....                  ```````                 " << endl;
+cout <<"                                        ``...--.---:-.`                 ....``` ```....`            " << endl;
+cout <<"                                             -//::-/`                `..`            `....          " << endl;
+cout <<"                                             -/:-.`-.               ..             `/ooo-`-`        " << endl;
+cout <<"                                             :/:.``.-             `-`             .+oooo.  ..       " << endl;
+cout <<"                                            .//:.```-.`           -      .`      -oooo/`    ..      " << endl;
+cout <<"                                        `.-://:.`````.:/:-.`     .`     .oo/`   -oooo:`      -      " << endl;
+cout <<"                                    `-:///////:-----://:://::-.` -       /oo/. -oooo-        .`     " << endl;
+cout <<"                                 `-////:::::::://://:::::::::://::       `+oo+-+ooo-         .`     " << endl;
+cout <<"                               `:///:::::::::::///:::::::::::::::/`       `+oo+ooo:          -`     " << endl;
+cout <<"                             `:/::::::::::::::/--/:::::::::::::::/-        -ooooo+`         `-      " << endl;
+cout <<"                            .///::::::::::::::/.`:/:::::::::::::::/.        /oooo.         `-       " << endl;
+cout <<"                           -//:::::::::::::://-`./:::::::::::::::::/-`      `++o:         ..        " << endl;
+cout <<"                         `:+/::::::::::::://-.``-///:://::::::::::::/:-`     ```        `.`         " << endl;
+cout <<"                        `:+/:::::::::::::/:.````...```:/:::::::::/+/:://:.``       ```..`           " << endl;
+cout <<"                        ://::::/+:::::::/:.``````````.//:::::::::/-+/:::://.`.....````              " << endl;
+cout <<"                       -//:::://////:///:.```````````.:/:::::::::/`-+/::::/-                        " << endl;
+cout <<"                      ./::::::/-///++:-.`````````````:/::::::::::/` :+/::::/`                       " << endl;
+cout <<"                     `/+/::::://:::+/:.....-....`.``.:/::::::::::/`  ///::::/                       " << endl;
+cout <<"                     `/+/////:/:////+//://////::----//:::::::::::/`  ./+::::/-                      " << endl;
+cout <<"                      -/++/++++/////+//////:////////:::::::::::::/`   ://::::/`                     " << endl;
+cout <<"                       `-://+////+///+++///////+//////////:::::::/`   `///:::/-                     " << endl;
+cout <<"                          `.----+::::--.-....```---:/--..:///////:-----:::////:-.                   " << endl;
+cout <<"                        .-.`                               ```                                       " << endl;
+cout <<"                                                                                                     " << endl;
+    system("pause");
+
+
+
+}
+
+
+
+void weared_mask() {
+
+    cout << " \t\t  Well done bro you are the real citizen of india who cares of nation and their family."  << endl;
+
+
+
+
+
+    cout << "             w           w  e e e e  L        L                              " << endl;
+
+    cout << "              w         w   e        L        L                      " << endl;
+
+    cout << "               w       w    e e e e  L        L                           " << endl;
+
+    cout << "                w  w  w     e        L        L                        " << endl;
+
+    cout << "                 w   w      e e e e  L L L L  L L L L                          " << endl;
+
+    cout << "                                                                " << endl;
+
+    cout << "              D D        O    N       N  E E E E                                 " << endl;
+
+    cout << "              D   D    O   O  N N     N  E                                  " << endl;
+
+    cout << "              D    D  O     O N   N   N  E E E E                                 " << endl;
+
+    cout << "              D   D    O   O  N     N N  E                                 " << endl;
+
+    cout << "              D D        O    N       N  E E E E                              " << endl;
+
+    cout << " ------------------------------------------------------------------------------------------------------                                                               " << endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+int main() {
+
+	system( "Color 5E ");
+
+
+
+	int age;
+
+	string mask;
+
+
+
+
+
+	cout << " \t\t  Enter your age " << endl;
+
+	cin >> age;
+
+
+
+	if (age >= 65) {
+
+		care();
+
+
+
+
+
+	}
+
+
+
+	else if (age <= 18) {
+
+
+
+		care();
+
+	}
+
+
+
+	else {
+
+
+
+		cout << " \t\t  Live your life after COVID as life will never comeback.\n  All the people who are going out unnecessarily in COVID must remember that your life is not too cheap to die like this bro.\n  Your life is made to succeed not to die because of a virus. " << endl;
+
+	}
+
+
+
+    cout << " ------------------------------------------------------------------------------------------------------                                                               " << endl;
+
+
+
+
+
+    cout << " \t\t  Please enter Yes if you wear mask and no is you dont ( not wearing not recommended ) " << endl;
+
+    cin >> mask;
+
+    cout << endl;
+
+
+
+	if ( mask == "Yes" || mask == "yes" ) {
+
+
+
+
+
+        weared_mask();
+
+
+
+	}
+
+
+
+    else{
+
+
+
+        nowear_mask();
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+
+
+
+}
+
